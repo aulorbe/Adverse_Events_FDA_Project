96c005eafe2535f72510d158a331832069813009
Anthony Tapias <41206609+anthonytapias@users.noreply.github.com>
2018-11-17 18:04:41 +0000

    update

---
+++ dash_package/dashboard.py
@@ -0,0 +1,9 @@
+from dash_package import app
+import dash_core_components as dcc
+import dash_html_components as html
+from dash_package.models import *
+from dash_package.routes import *
+
+
+
+app.layout = html.Div('hello world')
